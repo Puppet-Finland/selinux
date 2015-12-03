1a13fce3591d9109c67db405612c68d1b593ef09
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-12-03 15:25:19 +0200

    Add installation of selinux python utilities
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,4 +9,10 @@ class selinux::install inherits selinux::params {
         ensure => present,
         name   => $::selinux::params::lib_package_name,
     }
+
+    # Some Puppet modules also need the "semanage" command
+    package { 'selinux-semanage':
+        ensure => present,
+        name   => $::selinux::params::semanage_package_name,
+    }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -8,9 +8,11 @@ class selinux::params {
     case $::osfamily {
         'RedHat': {
             $lib_package_name = 'libselinux'
+            $semanage_package_name = 'policycoreutils-python'
         }
         'Debian': {
             $lib_package_name = 'libselinux1'
+            $semanage_package_name = 'policycoreutils'
         }
         default: {
             fail("Unsupported OS: ${::osfamily}")
